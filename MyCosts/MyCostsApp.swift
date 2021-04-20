c3f9ade191e08e553e127d0f0151fd66995994e1
Elena Kuklina <liafell.lansera@gmail.com>
2021-04-20 11:21:20 +0300

    Initial Commit

---
+++ MyCosts/MyCostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCostsApp.swift
+//  MyCosts
+//
+//  Created by  Elena Kuklina on 20.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyCostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
